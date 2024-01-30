a02297464b296b635a4512832cd0a027c219fc0f
Tarun Rout <tarunrout100@gmail.com>
2024-01-31 00:56:28 +0530

    World wise module -17, till video 7 (30.1.2024)

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -4,6 +4,7 @@ import Product from "./pages/Product";
 import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
 import Pagenotfound from "./pages/Pagenotfound";
+import AppLayout from "./pages/AppLayout";
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
           <Route path="/" element={<Homepage />} />
           <Route path="product" element={<Product />} />
           <Route path="pricing" element={<Pricing />} />
+          <Route path="app" element={<AppLayout />} />
           <Route path="*" element={<Pagenotfound />} />
         </Routes>
       </BrowserRouter>
--- worldwise/src/pages/Homepage.jsx
+++ worldwise/src/pages/Homepage.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import PageNav from "../components/PageNav"
+import PageNav from "../components/PageNav";
+import { Link } from "react-router-dom";
+
 export default function Homepage() {
   return (
     <div>
       <PageNav />
-      <div>Homepage</div>
+      <h1 className="test">WorldWise</h1>
+      <Link to="/app">Go to the app</Link>
     </div>
   );
 }
